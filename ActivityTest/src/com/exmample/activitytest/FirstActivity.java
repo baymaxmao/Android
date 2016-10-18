d24712c246c62c78f49bc90816fd86627ec015e5
sl.mao <maoshanli_wust@163.com>
2016-10-18 20:18:04 +0800

    2.2.3

---
+++ ActivityTest/src/com/exmample/activitytest/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.exmample.activitytest;
+
+
+import com.example.activitytest.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+protected void onCreate(Bundle savedInstanceState){
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.first_layout);
+}
+}
