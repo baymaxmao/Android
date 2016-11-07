9d4e6fb4926ef85d71d8878ad495655de8fb2c50
sl.mao <maoshanli_wust@163.com>
2016-11-07 10:57:32 +0800

    end Chapter5

---
+++ BroadcastBestPractice/src/com/example/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.broadcastbestpractice;
+import android.app.Activity;
+import android.os.Bundle;
+public class BaseActivity extends Activity{
+ protected void onCreate(Bundle savedInstanceState){
+	 super.onCreate(savedInstanceState);
+	 ActivityCollector.addActivity(this);
+ }
+ protected void onDestroy(){
+	 super.onDestroy();
+	 ActivityCollector.removeActivity(this);
+ }
+}
